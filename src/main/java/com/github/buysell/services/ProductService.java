e838fae90e4c7d7148b3fcce06fb2d5beb51a29a
4veg0s <tkachyov03@inbox.ru>
2024-06-23 20:27:41 +0300

    integrated PostrgreSQL db for storing products

--- src/main/java/com/github/buysell/services/ProductService.java
+++ src/main/java/com/github/buysell/services/ProductService.java
@@ -1,38 +1,37 @@
 package com.github.buysell.services;
 
 import com.github.buysell.models.Product;
+import com.github.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RequiredArgsConstructor
+@Slf4j
 @Service
 public class ProductService {
+    private final ProductRepository productRepository;
     private List<Product> products = new ArrayList<>();
-    private long ID = 0;
 
-    {
-        products.add(new Product(++ID, "PlayStation 5", "Wet dream", 67000, "Saint-P", "4veg0s"));
-        products.add(new Product(++ID, "iPhone 15", "Girls dream", 100000, "Moscow", "s0gev4"));
-        products.add(new Product(++ID, "HUAWEI D15 notebook", "Does it's job well", 40000, "Saint-P", "4veg0s"));
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-    public List<Product> listProducts() { return products; }
-
     public  void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;    // FIXME: add optional?
+        return productRepository.findById(id).orElse(null);
     }
 }
