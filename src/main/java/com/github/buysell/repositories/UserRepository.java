0a797d69218ec275ff73cc484ba09be9fc690291
4veg0s <tkachyov03@inbox.ru>
2024-06-28 17:25:17 +0300

    added spring security

---
+++ src/main/java/com/github/buysell/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.github.buysell.repositories;
+
+import com.github.buysell.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
